b577af4189a7e07291366274da687fcd395ea8d9
Anton Yermakovich <antonyermakovich@gmail.com>
2025-11-01 22:31:46 +0300

    add live section

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const burger = document.querySelector(".header__icon");
+const slider = document.querySelector(".slider");
+const range = document.querySelector(".slider__range input");
 
 burger.addEventListener("click", openMenu);
 
@@ -7,3 +9,7 @@ function openMenu() {
     ? this.classList.remove("header__icon_active")
     : this.classList.add("header__icon_active");
 }
+
+range.addEventListener('input', () => {
+  slider.style.setProperty('--value', range.value + '%')
+})
